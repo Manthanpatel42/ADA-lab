1b677159e84b28c17b844bb5229e794833dbbebc
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-05-01 18:37:04 +0530

    Add files via upload

---
+++ prac_13.java
@@ -0,0 +1,25 @@
+public class SCS {
+
+    public static void main(String[] args) {
+        String a = "AGGTAB";
+        String b = "GXTXAYB";
+
+        int dp[][] = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (a.charAt(i - 1) == b.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println("SCS Length: " + dp[a.length()][b.length()]);
+    }
+}
