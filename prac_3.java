56dcfe375238c0dd889c7de8cdcadf1cbe99e8b5
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-05-01 18:32:51 +0530

    Add files via upload

---
+++ prac_3.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        int n = arr.length;
+
+        long start = System.nanoTime();
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        long end = System.nanoTime();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nTime: " + (end - start));
+    }
+}
