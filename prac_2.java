f896c7480309860d584aafd9ce597a5541e3b357
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-05-01 18:32:28 +0530

    Add files via upload

---
+++ prac_2.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        int n = arr.length;
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        long end = System.nanoTime();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nTime: " + (end - start));
+    }
+}
